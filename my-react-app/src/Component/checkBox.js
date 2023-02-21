b565d4fc9a0757b634e9fd52d6f8628af0444542
Hattori-Takuma <hattori.ta@icloud.com>
2023-02-22 01:45:02 +0900

    チェックボックス

--- my-react-app/src/Component/checkBox.js
+++ my-react-app/src/Component/checkBox.js
@@ -1,33 +1,27 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Store } from '../store';
 
-//checkboxコンポーネント
-const CheckBox = ({ id, value, checked, onChange }) => {
-  return (
-    <input
-      id={id}
-      type="checkbox"
-      name="inputNames"
-      checked={checked}
-      onChange={onChange}
-      value={value}
-    />
-  )
-}
 export const CheckBoxList = () => {
+
+  const [prefData, setPrefData] = useState([]);
+  const [checkedItems, setCheckedItems] = useState({});
+
   const { globalState } = useContext(Store);
 
   console.log(globalState, 'globalState check***')
 
-  const [checkedItems, setCheckedItems] = useState({})
+  useEffect(() => {
+    if (globalState.hasOwnProperty('result_data')) {
+      setPrefData(globalState.result_data);
+    }
+  }, [globalState]);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     //checkedItemsのstateをセット
     setCheckedItems({
       ...checkedItems,
       [e.target.id]: e.target.checked
     })
-    console.log('checkedItems:', checkedItems)
   }
   return (
     <>
@@ -47,6 +41,20 @@ export const CheckBoxList = () => {
             </label>
           )
         })} */}
+
+        {prefData.map((item, index) => {
+          return (
+            <div key={index}>
+              <label htmlFor={item.prefCode}>{item.prefName}</label>
+              <input
+                id={item.prefCode}
+                type="checkbox"
+                name="inputNames"
+                onChange={handleChange}
+              />
+            </div>
+          );
+        })}
       </>
 
     </>
